82fac8fde9017a50e6445174866b1e3bd9a354a9
JYOTI PRAKASH BEHERA <105623394+jyotiprakashb23@users.noreply.github.com>
2024-07-30 12:17:54 +0530

    nothing

--- src/navigation/BottomNav.js
+++ src/navigation/BottomNav.js
@@ -1,69 +1,80 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Home from '../pages/content/Home';
-import ServiceRequest from '../pages/content/ServiceRequest';
-import Account from '../pages/content/Account';
+import Home from "../pages/content/Home";
+import ServiceRequest from "../pages/content/ServiceRequest";
+import Account from "../pages/content/Account";
 
-import CustomIcon from '../components/Basic/CustomIcon';
-import { appColorPalette } from '../utilities/constants'
+import CustomIcon from "../components/Basic/CustomIcon";
+import { appColorPalette } from "../utilities/constants";
 const Tab = createBottomTabNavigator();
 
 function MyTabs() {
-    return (
-        <Tab.Navigator
-            screenOptions={{
-                "tabBarHideOnKeyboard": true,
-                tabBarStyle: { backgroundColor: appColorPalette.primaryBLACK }
-            }}
-        >
-            <Tab.Screen options={{
-                headerShown: false,
-                tabBarLabelStyle: {
-                    fontSize: 13,
-                    fontWeight: 'bold'
-                },
-                tabBarIcon: ({ color, focused }) => (
-                    <CustomIcon
-                        name="home"
-                        color={focused ? appColorPalette.primaryRED : color}
-                        size={26}
-                    />
-                ),
-                tabBarActiveTintColor: 'white',
-            }}
-                name="Home" component={Home} />
-            <Tab.Screen options={{
-                headerShown: false,
-                tabBarLabelStyle: {
-                    fontSize: 13,
-                    fontWeight: 'bold'
-                },
-                tabBarIcon: ({ color, focused }) => (
-                    <CustomIcon
-                        name="wpforms"
-                        color={focused ? appColorPalette.primaryRED : color}
-                        size={26}
-                    />
-                ),
-                tabBarActiveTintColor: 'white',
-            }} name="Request Service" component={ServiceRequest} />
-            <Tab.Screen options={{
-                headerShown: false,
-                tabBarLabelStyle: {
-                    fontSize: 13,
-                    fontWeight: 'bold'
-                },
-                tabBarIcon: ({ color, focused }) => (
-                    <CustomIcon
-                        name="user"
-                        color={focused ? appColorPalette.primaryRED : color}
-                        size={26}
-                    />
-                ),
-                tabBarActiveTintColor: 'white',
-            }} name="Account" component={Account} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarHideOnKeyboard: true,
+        tabBarStyle: { backgroundColor: appColorPalette.primaryBLACK },
+      }}
+    >
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontSize: 13,
+            fontWeight: "bold",
+          },
+          tabBarIcon: ({ color, focused }) => (
+            <CustomIcon
+              name="home"
+              color={focused ? appColorPalette.primaryRED : color}
+              size={26}
+            />
+          ),
+          tabBarActiveTintColor: "white",
+        }}
+        name="Home"
+        component={Home}
+      />
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontSize: 13,
+            fontWeight: "bold",
+          },
+          tabBarIcon: ({ color, focused }) => (
+            <CustomIcon
+              name="wpforms"
+              color={focused ? appColorPalette.primaryRED : color}
+              size={26}
+            />
+          ),
+          tabBarActiveTintColor: "white",
+        }}
+        name="Request Service"
+        component={ServiceRequest}
+      />
+      <Tab.Screen
+        options={{
+          headerShown: false,
+          tabBarLabelStyle: {
+            fontSize: 13,
+            fontWeight: "bold",
+          },
+          tabBarIcon: ({ color, focused }) => (
+            <CustomIcon
+              name="user"
+              color={focused ? appColorPalette.primaryRED : color}
+              size={26}
+            />
+          ),
+          tabBarActiveTintColor: "white",
+        }}
+        name="Account"
+        component={Account}
+      />
+    </Tab.Navigator>
+  );
 }
 
-export default MyTabs
+export default MyTabs;
